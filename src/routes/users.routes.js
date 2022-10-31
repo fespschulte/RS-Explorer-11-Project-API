4019331484cf55215eae32b7bcefd9d6a8365642
Fernando Pinto <fschultepinto@gmail.com>
2022-10-31 12:48:53 -0300

    Finalização da rota users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,9 +2,11 @@ const { Router } = require("express")
 
 const usersRoutes = Router()
 
-app.post("/users", (request, response) => {
+usersRoutes.post("/", (request, response) => {
   const {name, email, password } = request.body
 
 
   response.send(`Usuário: ${name} - E-mail: ${email} - Senha: ${password}`)
 })
+
+module.exports = usersRoutes
