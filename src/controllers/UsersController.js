d505095d6f7e2b920b44a14c2ac3d6691d550e22
Fernando Pinto <fschultepinto@gmail.com>
2022-10-31 21:02:14 -0300

    rota

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,20 @@
+class UsersController {
+
+  /* No máximo 5 métodos
+   index - GET para listar vários registros
+   show - GET para exibir um registro específico
+   create - POST para criar um registro
+   update - PUT para atualizar um registro
+   delete - DELETE para remover um registro
+  */
+
+  create(request, response) {
+    const {name, email, password } = request.body
+
+
+    response.json({ name, email, password })
+  }
+
+}
+
+module.exports = UsersController
