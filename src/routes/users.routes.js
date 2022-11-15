9ca6f73e88b7070db1de45a3e1b2e353aa98ca7b
Fernando Pinto <fschultepinto@gmail.com>
2022-11-15 10:35:05 -0300

    Implementação de middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,15 @@
 const { Router } = require("express") 
 const UsersController = require("../controllers/UsersController")
 const usersRoutes = Router()
+
+function myMiddleware(request, response, next) {
+  
+  next()
+
+}
+
 const usersController = new UsersController()
 
-usersRoutes.post("/", usersController.create)
+usersRoutes.post("/", myMiddleware, usersController.create)
 
 module.exports = usersRoutes
