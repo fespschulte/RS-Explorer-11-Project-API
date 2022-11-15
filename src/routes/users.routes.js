8d3d607026667339b244968319a0739db2dac43a
Fernando Pinto <fschultepinto@gmail.com>
2022-11-15 12:50:00 -0300

    Instalação do express-async-errors e tratando exceções

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,14 +2,16 @@ const { Router } = require("express")
 const UsersController = require("../controllers/UsersController")
 const usersRoutes = Router()
 
-function myMiddleware(request, response, next) {
-  
-  next()
+// function myMiddleware(request, response, next) {
+//   if(!request.body.isAdmin)
+//     return 
 
-}
+//   next()
+
+// }
 
 const usersController = new UsersController()
 
-usersRoutes.post("/", myMiddleware, usersController.create)
+usersRoutes.post("/", /*myMiddleware,*/ usersController.create)
 
 module.exports = usersRoutes
